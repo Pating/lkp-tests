10f1426244d9cc518d54b96ed1f9945dcc2cb499
Yilong Ren <yilongx.ren@intel.com>
2016-09-02 13:55:30 +0800

    bin/post-run: move upload_files() to lib/result.sh
    Cc: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Yilong Ren <yilongx.ren@intel.com>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>

--- lib/result.sh
+++ lib/result.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. $LKP_SRC/lib/env.sh
+
 export RESULT_MNT='/result'
 export RESULT_PATHS='/lkp/paths'
 
@@ -117,4 +119,41 @@ cleanup_path_record_from_result_root()
 	done
 
 	rm -f $dot_temp_file
+}
+
+is_local_server()
+{
+	[ "$LKP_SERVER" != "${LKP_SERVER#inn}" ] && return
+	[ "$LKP_SERVER" != "${LKP_SERVER#192.168.}" ] && return
+	return 1
+}
+
+upload_files()
+{
+	local file
+	local ret=0
+
+	if has_cmd rsync && is_local_server; then
+		rsync -a --ignore-missing-args --min-size=1 "$@" rsync://$LKP_SERVER$JOB_RESULT_ROOT/
+	elif has_cmd curl; then
+		for file
+		do
+			[ -s "$file" ] || continue
+			curl -T $file http://$LKP_SERVER$JOB_RESULT_ROOT/ || ret=$?
+		done
+		return $ret
+	else
+		# NFS is the last resort -- it seems unreliable, either some
+		# content has not reached NFS server during post processing, or
+		# some files occasionally contain some few '\0' bytes.
+		for file
+		do
+			[ -s "$file" ] || continue
+			chown lkp.lkp "$file"
+			chmod ug+w    "$file"
+			cp -p "$file" $RESULT_ROOT/ || ret=$?
+		done
+		[ "$ret" -ne 0 ] && ls -l "$@" $RESULT_ROOT 2>&1
+		return $ret
+	fi
 }
